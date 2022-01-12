c71469d5b4f8d3e1d8c1aa9dead1cba9cece445d
NiverMtz <nivermartinezmed@gmail.com>
2022-01-11 20:35:24 -0600

    Test: triggers and Refactoring

--- 03-fundamentals/tests/unit/components/counter.spec.js
+++ 03-fundamentals/tests/unit/components/counter.spec.js
@@ -3,6 +3,12 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallowMount( Counter );
+    })
+
     // test('debe hacer match con el snapshot', () => {
 
     //     const wrapper = shallowMount(Counter)
@@ -12,14 +18,39 @@ describe('Counter Component', () => {
 
     test('h2 debe de tener el valor por defecto', () => {
 
-        const wrapper = shallowMount( Counter )
-
         expect(wrapper.exists('h2')).toBeTruthy()
 
         const h2Value = wrapper.find('h2').text()
 
         expect(h2Value).toBe('Counter')
 
+    })
+
+    test('El valor por defecto debe ser 100 en el p', () => {
+
+        // pTags
+        // const pTags = wrapper.findAll('p')
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        // expect segundo p === 100
+        // expect(pTags[1].text()).toBe('100')
+        expect(value).toBe('100')
+    })
+
+    test('Debe de incrementar y decremetar el contador', async () => {
+
+        const [ increaseBtn, decreaseBtn ] = wrapper.findAll('button')
+
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        expect( value ).toBe('101')
     })
 
 })
