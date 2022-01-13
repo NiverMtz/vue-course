cd9a5e79fa5d008cdad80b22cda2803632be130a
NiverMtz <nivermartinezmed@gmail.com>
2022-01-13 02:15:38 -0600

    pokemon app directives

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>¿Quién es este pokemón?</h1>
+
+    <!-- TODO: img -->
+    <PokemonPicture :pokemonId="300" :showPokemon="true"/>
+
+    <!-- TODO: options -->
+    <PokemonOptions />
+
+</template>
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonPicture,
+        PokemonOptions
+  }
+}
+</script>
+<style>
+    
+</style>
