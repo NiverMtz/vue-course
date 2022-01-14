42b062e11d434ff7adbb9895fe826c0a3bf7ad32
NiverMtz <nivermartinezmed@gmail.com>
2022-01-14 02:39:08 -0600

    options complete

--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -5,18 +5,35 @@
     <PokemonPicture :pokemonId="300" :showPokemon="true"/>
 
     <!-- TODO: options -->
-    <PokemonOptions />
+    <PokemonOptions :pokemons="pokemonArr" />
 
 </template>
 <script>
 import PokemonPicture from '@/components/PokemonPicture.vue'
 import PokemonOptions from '@/components/PokemonOptions.vue'
 
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+// console.log( getPokemonOptions() )
+
 export default {
     name: 'PokemonPage',
     components: {
         PokemonPicture,
         PokemonOptions
+  },
+  data() {
+      return {
+          pokemonArr: []
+      }
+  },
+  methods: {
+      async mixPokemonArray() {
+          this.pokemonArr = await getPokemonOptions()
+      }
+  },
+  mounted() {
+      this.mixPokemonArray()
   }
 }
 </script>
